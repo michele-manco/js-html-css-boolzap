b0d796a990a21a32f041a0e5a927d4645707a307
Michele_Manco <motlamik@gmail.com>
2019-12-06 12:27:06 +0100

    input + click working

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.footright').click(function(){
+    msg_go();
+  });
+  $('.write').keypress(function(event){
+    if (event.which == 13) {
+
+    msg_go();
+    }
+
+
+  });
+});
+function msg_go(){
+  var text_msg = $('.write').val();
+
+  if(text_msg.length != 0) {
+    var new_msg = $('.template .message').clone();
+    new_msg.find('.message-text').text(text_msg);
+    console.log(new_msg);
+    new_msg.addClass('sent');
+    $('.right-mess.active').append(new_msg);
+    $('.write').val('');
+  }
+
+}
